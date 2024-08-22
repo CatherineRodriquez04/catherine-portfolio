9eabd68e29003b076e44475ce4c4393c71f6e204
CatherineRodriquez04 <114537594+CatherineRodriquez04@users.noreply.github.com>
2024-08-22 02:17:58 -0500

    changed icon image

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -94,7 +94,7 @@ const Contact = () => {
                     {/* form */}
                     <div className="xl:w-[54%] order-2 xl:order-none ">
                         <form onSubmit={handleSubmit} className="flex flex-col gap-6 p-10 bg-[#565f69] rounded-xl">
-                            <h3 className="text-4xl text-accent">Let&apos;s Connect!</h3>
+                            <h3 className="text-4xl text-accent">Let&apos;runs Connect!</h3>
                             <p className="text-white/80">Feel free to reach out with any questions or comments.</p>
                             {/* input */}
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
